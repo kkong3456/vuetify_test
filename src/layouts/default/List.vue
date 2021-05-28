5b0de0585eb445c39a4283266109b89b296b8f76
kkongk <kkong3456@gmail.com>
2021-05-28 10:59:44 +0900

    multilayout 완성

--- src/layouts/default/List.vue
+++ src/layouts/default/List.vue
@@ -1,23 +1,39 @@
 <template>
   <v-list
     dense
+    expand
     nav
   >
-    <default-list-item :items="items"></default-list-item>
+    <template
+      v-for="(item, index) in items"
+    >
+      <default-list-group
+        v-if="item.items"
+        :key="`group-${index}`"
+        :item="item"
+      />
+      <default-list-item
+        v-else
+        :key="`item-${index}`"
+        :item="item"
+      />
+    </template>
   </v-list>
 </template>
 
 <script>
 import DefaultListItem from './ListItem'
+import DefaultListGroup from './ListGroup'
 export default {
   name:"DefaultList",
   components:{
     DefaultListItem,
+    DefaultListGroup,
   },
   props:{
     items:{
-      // type:Array,
-      // default:()=>([]),
+      type:Array,
+      default:()=>([]),
     }
   }
 }
--- src/layouts/default/ListItem.vue
+++ src/layouts/default/ListItem.vue
@@ -1,35 +1,28 @@
 <template>
-<v-container>
-   <v-list-item
-      v-for="item in items"
-      :key="item.title"
-      link
-      :to="item.to"
-      active-class="primary"
-      class="py-1"
-    >
-      <v-list-item-icon>
-        <v-icon>{{ item.icon }}</v-icon>
-      </v-list-item-icon>
-
-      <v-list-item-content>
-        <v-list-item-title class="text-h6 py-2 red--text text--lighten-3 font-weight-black">
-          {{ item.title }}
-        </v-list-item-title>
-      </v-list-item-content>
-    </v-list-item>
-
-</v-container>
-
+  <v-list-item
+    link
+    :to="item.to"
+    class="py-1"
+    active-class="primary white--text"
+  >
+    <v-list-item-icon>
+      <v-icon>{{ item.icon }}</v-icon>
+    </v-list-item-icon>
+    <v-list-item-content>
+      <v-list-item-title>{{ item.title }}</v-list-item-title>
+    </v-list-item-content>
+  </v-list-item>
 </template>
 <script>
 export default {
-  name:"DefaultListItem",
-  props:{
-    items:{}
+  name: 'DefaultListItem',
+  props: {
+    item: {
+      type: Object,
+      default: () => ({})
+    }
   }
 }
-
 </script>
 
 <style scoped>
